b6b3767a329a8408a4a26add81694e3c291fd990
NiceGent <108413327+NiceGent@users.noreply.github.com>
2022-07-30 09:24:40 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const colorFour = document.getElementById("color-four");
 const colorFive = document.getElementById("color-five");
 const colorSix = document.getElementById("color-six");
 const colorSeven = document.getElementById("color-seven");
+const colorArray = [colorOne, colorTwo, colorThree, colorFour, colorFive, colorSix, colorSeven];
 
 const playBtn = document.getElementById("play-btn");
 const stopBtn = document.getElementById("stop-btn");
@@ -12,41 +13,37 @@ const colorBtn = document.getElementById("color-btn");
 
 let dancinglights = null;
 
-playBtn.addEventListener("click", function() {
-    dancinglights = setInterval(function dancingLights() {
-        colorOne.style.height = randomHeight() + `0%`;
-        colorTwo.style.height = randomHeight() + `0%`;
-        colorThree.style.height = randomHeight() + `0%`;
-        colorFour.style.height = randomHeight() + `0%`;
-        colorFive.style.height = randomHeight() + `0%`;
-        colorSix.style.height = randomHeight() + `0%`;
-        colorSeven.style.height = randomHeight() + `0%`;
-    }, 200) 
-});
+playBtn.addEventListener("click", renderEqualizer);
 
+function renderEqualizer() {
+    playBtn.removeEventListener("click", renderEqualizer);
+    dancinglights = setInterval(function dancingLights() {
+        for(i = 0; i < colorArray.length; i ++) {
+            colorArray[i].style.height = randomHeight();
+            console.log(lineHeight)
+        }
+    }, 200)
+}
 
 stopBtn.addEventListener("click", function() {
     clearInterval(dancinglights);
-})
+    playBtn.addEventListener("click", renderEqualizer);
+}) 
 
 function randomHeight() {
-    return lineHeight = Math.floor(Math.random() * 10) + 1;
+    return lineHeight = `${(Math.floor(Math.random() * 10) + 1)}0%`;
 };
 
 colorBtn.addEventListener("click", function(){
-    let colourSetOneRed = ["#DAF7A6", "#FFC300", "#FFA07A","#FF5733", "#C70039", "#900C3F", "#581845"];
-    let colourSetTwoBlue = ["#191970", "#0000ff", "#add8e6", "#b0c4de", "#87cefa", "#87ceeb", "#6495ed"];
-    let colourSetThreeGreen = ["#008000", "#98fb98", "#90ee90", "#8fbc8f", "#adff2f", "#7fff00", "#00ff00"];
-    let colourSetRainbow = ["purple", "blue", "cyan", "green", "yellow", "orange", "red"];
+    const colourSetOneRed = ["#DAF7A6", "#FFC300", "#FFA07A","#FF5733", "#C70039", "#900C3F", "#581845"];
+    const colourSetTwoBlue = ["#191970", "#0000ff", "#add8e6", "#b0c4de", "#87cefa", "#87ceeb", "#6495ed"];
+    const colourSetThreeGreen = ["#008000", "#98fb98", "#90ee90", "#8fbc8f", "#adff2f", "#7fff00", "#00ff00"];
+    const colourSetRainbow = ["purple", "blue", "cyan", "green", "yellow", "orange", "red"];
 
-    let lightColours = [colourSetOneRed, colourSetTwoBlue, colourSetThreeGreen, colourSetRainbow];
+    const lightColours = [colourSetOneRed, colourSetTwoBlue, colourSetThreeGreen, colourSetRainbow];
     let randomLightColours = lightColours[Math.floor(Math.random() * lightColours.length)];
 
-    colorOne.style.backgroundColor = randomLightColours[0];
-    colorTwo.style.backgroundColor = randomLightColours[1];
-    colorThree.style.backgroundColor = randomLightColours[2];
-    colorFour.style.backgroundColor = randomLightColours[3];
-    colorFive.style.backgroundColor = randomLightColours[4];
-    colorSix.style.backgroundColor = randomLightColours[5];
-    colorSeven.style.backgroundColor = randomLightColours[6];
+    for(i = 0; i < colorArray.length; i++) {
+        colorArray[i].style.backgroundColor = randomLightColours[i];
+    }
 });
